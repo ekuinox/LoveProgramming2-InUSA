7ad710bf858e0ae1d9762ffbc58e37fc592d6845
ekuinox <depkey@me.com>
2020-02-21 13:34:16 +0900

    Event interface

---
+++ Assets/Scripts/LoveEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface LoveEvent
+{
+    public void init(Dictionary<string, string> args);
+    public void run();
+}
