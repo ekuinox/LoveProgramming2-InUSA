3307422d517595751cc679a4072f673080b741b4
ekuinox <depkey@me.com>
2020-02-21 13:53:26 +0900

    試しにラブが減るイベントと称して作った

---
+++ Assets/Scripts/DecreaseLove.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DecreaseLoveEvent : LoveEvent
+{
+    int count = 0;
+    public void init(Dictionary<string, string> args)
+    {
+        var count = "0";
+        args.TryGetValue("count", out count);
+        this.count = int.Parse(count);
+    }
+
+    public void run()
+    {
+        UnityEngine.Debug.Log($"{count} 減らしちゃるぞ");
+    }
+
+}
