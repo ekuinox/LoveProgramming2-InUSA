5957006d18c56f7c8f87d38eeed30912e9bafdad
ekuinox <depkey@me.com>
2020-02-21 10:36:58 +0900

    メッセージを格納する1構造体を定義

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    struct Message
+    {
+        [Serializable]
+        public enum Type
+        {
+            Normal,
+            Selection,
+        }
+
+        public Type type;
+        public string text; // if Type::Normal
+        public string[] selections; // if Type::Selection
+    }
+}
