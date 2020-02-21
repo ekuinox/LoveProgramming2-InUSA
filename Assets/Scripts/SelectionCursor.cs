22ad81f6c281ad85c77094ce1ada7aefc2f51471
ekuinox <depkey@me.com>
2020-02-21 15:13:04 +0900

    カーソルをあーだこうだする

---
+++ Assets/Scripts/SelectionCursor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// 選択肢をやるためのカーソル
+/// </summary>
+public class SelectionCursor : MonoBehaviour
+{
+    [SerializeField] private int currentSelected = 0;
+    [SerializeField] private int padding = 50;
+
+    private int selectionLength = 3;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            currentSelected += 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            currentSelected -= 1;
+        }
+
+        // clamp
+        if (currentSelected < 0) currentSelected = 0;
+        if (currentSelected > selectionLength - 1) currentSelected = selectionLength - 1;
+    }
+
+    public int getCurrentSelected()
+    {
+        return currentSelected;
+    }
+}
