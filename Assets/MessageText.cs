3cddef34600d2bae5de16d717ca706434c127c5d
ekuinox <depkey@me.com>
2020-02-21 14:44:48 +0900

    Messageを弄るために

---
+++ Assets/MessageText.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+///  メッセージテキストを弄る
+/// </summary>
+public class MessageText : MonoBehaviour
+{
+    /// <summary>
+    /// 表示するテキストのid
+    /// </summary>
+    static public int textId = 0;
+
+    static private Message[] messages;
+
+    private Message message
+    {
+        get
+        {
+            return messages[textId];
+        }
+    }
+
+    void Start()
+    {
+        LoadJson();
+        var textComponent = gameObject.GetComponent<Text>();
+        textComponent.text = message.text;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    static public void LoadJson(string path = null)
+    {
+        messages = MessageLoader.GetFromFile(path ?? $"{Application.dataPath}/messages.json").ToArray();
+    }
+}
