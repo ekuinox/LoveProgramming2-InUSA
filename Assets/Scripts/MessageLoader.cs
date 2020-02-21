017b7c2b2324614090b6745217d97c5425b31441
ekuinox <depkey@me.com>
2020-02-21 11:21:52 +0900

    かなしいことにトップレベルが配列であることは許されなかった

--- Assets/Scripts/MessageLoader.cs
+++ Assets/Scripts/MessageLoader.cs
@@ -1,17 +1,22 @@
 ﻿using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
-using Assets.Scripts;
 
 public static class MessageLoader
 {
+    [System.Serializable]
+    struct Messages
+    {
+        public Message[] messages;
+    }
+
     public static List<Message> GetFromJson(string json)
     {
-        var messages = JsonUtility.FromJson<Message[]>(json);
+        var messages = JsonUtility.FromJson<Messages>(json);
 
         var messageList = new List<Message>();
 
-        foreach (Message message in messages)
+        foreach (Message message in messages.messages)
         {
             messageList.Add(message);
         }
