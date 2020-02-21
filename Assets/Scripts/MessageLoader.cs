8eaebe1533a425b23dc02788f1511d5064d0b100
ekuinox <depkey@me.com>
2020-02-21 10:37:24 +0900

    Messageのリストを読み込む関数を作成

---
+++ Assets/Scripts/MessageLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public static class MessageLoader
+{
+    static List<Message> GetFromJson(string json)
+    {
+        var messages = JsonUtility.FromJson<Message[]>(json);
+
+        var messageList = new List<Message>();
+
+        foreach (Message message in messages)
+        {
+            messageList.Add(message);
+        }
+
+        return messageList;
+    }
+
+    static List<Message> GetFromFile(string filePath)
+    {
+        try
+        {
+            var json = File.ReadAllText(filePath);
+            return GetFromJson(json);
+        }
+        catch {
+            return new List<Message>();
+        }
+    }
+}
