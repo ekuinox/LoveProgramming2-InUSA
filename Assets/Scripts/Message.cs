f7f1a35aad07725b098048ebe213eac7523a6fe5
ekuinox <depkey@me.com>
2020-02-21 12:02:17 +0900

    自身のidと次のid

--- Assets/Scripts/Message.cs
+++ Assets/Scripts/Message.cs
@@ -3,6 +3,8 @@
 [Serializable]
 public struct Message
 {
+    public int id;
+    public int nextId;
     public int type;
     public string text; // if Type::Normal
     public string[] selections; // if Type::Selection
