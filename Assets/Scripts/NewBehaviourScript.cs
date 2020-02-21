ac19f4fd3d35a037764d1a30ed59122062bd52a4
ekuinox <depkey@me.com>
2020-02-21 13:54:53 +0900

    試したいがためだけのスクリプト

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Newtonsoft.Json;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Serializable]
+    struct Messages
+    {
+        public Message[] messages;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        var messages = MessageLoader.GetFromFile(Application.dataPath + "/messages.json");
+        messages.ForEach(message =>
+        {
+            Debug.Log($"type => {message.type}, text => {message.text}");
+        });
+
+        {
+            var eventsData = EventLoader.GetFromFile(Application.dataPath + "/events.json");
+            var decreaseLove = EventLoader.SpawnEventById("decreaseLove");
+            decreaseLove.run();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
