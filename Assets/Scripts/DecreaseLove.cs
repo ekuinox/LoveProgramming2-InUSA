3c6bb55751219a4577c9b7c2bdb0006509fd6a58
ekuinox <depkey@me.com>
2020-02-22 10:21:55 +0900

    タゲに寄って好感度をへらす

--- Assets/Scripts/DecreaseLove.cs
+++ Assets/Scripts/DecreaseLove.cs
@@ -7,16 +7,33 @@ using System.Threading.Tasks;
 public class DecreaseLoveEvent : LoveEvent
 {
     int count = 0;
+    int target = 0;
     public void init(Dictionary<string, string> args)
     {
         var count = "0";
         args.TryGetValue("count", out count);
         this.count = int.Parse(count);
+
+        var target = "0";
+        args.TryGetValue("target", out target);
+        this.target = int.Parse(target);
     }
 
     public void run(MessageText messageText)
     {
-        UnityEngine.Debug.Log($"{count} 減らしちゃるぞ");
+        switch (target)
+        {
+            case 0:
+                Manager.unityLovePoint -= count;
+                UnityEngine.Debug.Log($"unityの好感度{count} 減らしちゃるぞ");
+                break;
+            case 1:
+                Manager.ue4LovePoint -= count;
+                UnityEngine.Debug.Log($"ue4の好感度{count} 減らしちゃるぞ");
+                break;
+            default:
+                break;
+        }
     }
 
 }
