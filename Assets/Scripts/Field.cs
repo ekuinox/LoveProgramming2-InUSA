23b0a3017177e6ecddb8fd3cc08e9c1a470d036c
takuteku <taku.0927.topublish@gmail.com>
2020-02-22 11:33:09 +0900

    プレイヤーの当たり判定を作った(今は壁だけ)

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Text.RegularExpressions;
+using System.Text;
 
 public enum EChipType
 {
@@ -24,7 +25,6 @@ public class Field : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     /*------------------------------------------
@@ -32,13 +32,24 @@ public class Field : MonoBehaviour
      * x,y              : 次に移動する座標値
      * 戻り値           : 次のチップのタイプ
      -------------------------------------------*/
-    public int GetNextChipType(int x, int y)
+    public EChipType GetNextChipType(int x, int y)
     {
+        int type;
         Tile tile = tileMap.GetTile<Tile>(new Vector3Int(x, y, 0));
-        string str = Regex.Replace(tile.sprite.name, @"[^0-9]", "");
-        int type = int.Parse(str);
 
-        return (int)GetChipType(type);
+        OutPutSpriteType(-1, 0);
+
+        if (tile != null)
+        {
+            string str = Regex.Replace(tile.sprite.name, @"[^0-9]", "");
+            type = int.Parse(str);
+        }
+        else
+        {
+            return EChipType.eNone;
+        }
+
+        return GetChipType(type);
     }
 
 
@@ -59,4 +70,67 @@ public class Field : MonoBehaviour
                 return EChipType.eNone;
         }
     }
+
+
+    private void OutPutSpriteType(int x, int y)
+    {
+        // 使われているSpriteをリストアップ
+        var bound = tileMap.cellBounds;
+        var spriteList = new List<Sprite>();
+        //for (int y = bound.max.y - 1; y >= bound.min.y; --y)
+        //{
+        //    for (int x = bound.min.x; x < bound.max.x; ++x)
+        //    {
+        //        var tile = tileMap.GetTile<Tile>(new Vector3Int(x, y, 0));
+        //        if (tile != null && !spriteList.Contains(tile.sprite))
+        //        {
+        //            spriteList.Add(tile.sprite);
+        //        }
+        //    }
+        //}
+
+        var tile = tileMap.GetTile<Tile>(new Vector3Int(x, y, 0));
+        if (tile != null && !spriteList.Contains(tile.sprite))
+        {
+            spriteList.Add(tile.sprite);
+        }
+
+        // どの場所でそのSpriteが使われているかを出力
+        var builder = new StringBuilder();
+        //for (int y = bound.max.y - 1; y >= bound.min.y; --y)
+        //{
+        //    for (int x = bound.min.x; x < bound.max.x; ++x)
+        //    {
+        //        var tile = tileMap.GetTile<Tile>(new Vector3Int(x, y, 0));
+        //        if (tile == null)
+        //        {
+        //            builder.Append("_,");
+        //        }
+        //        else
+        //        {
+        //            //var index = spriteList.IndexOf(tile.sprite);
+        //            string str = Regex.Replace(tile.sprite.name, @"[^0-9]", "");
+        //            int index = int.Parse(str);
+        //            builder.Append(index + ",");
+        //        }
+        //    }
+        //    builder.Append("\n");
+        //}
+
+        var tile2 = tileMap.GetTile<Tile>(new Vector3Int(x, y, 0));
+        if (tile2 == null)
+        {
+            builder.Append("_,");
+        }
+        else
+        {
+            //var index = spriteList.IndexOf(tile.sprite);
+            string str = Regex.Replace(tile2.sprite.name, @"[^0-9]", "");
+            int index = int.Parse(str);
+            builder.Append(index + ",");
+        }
+
+        Debug.Log(builder.ToString());
+    }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using System.Text;
-using System.Text.RegularExpressions;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,13 +8,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Field field;
     //[SerializeField] private Tilemap tilemap; 
 
-    private float moveX = 0, moveY = 0;
+    private float offsetX = 0.5f, offsetY = 0.5f;
+    private int moveX = 0, moveY = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -27,68 +24,42 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Down Arrow"))
         {
             Debug.Log("下");
-            //this.gameObject.transform.position += new Vector3(0, -1, 0);
+
+            if (field.GetNextChipType(moveX, moveY - 1) == EChipType.eNone)
+            {
+                moveY--;
+            }
             this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
         }
         else if (Input.GetButtonDown("Up Arrow"))
         {
             Debug.Log("上");
-            //this.gameObject.transform.position += new Vector3(0, 1, 0);
+            if (field.GetNextChipType(moveX, moveY + 1) == EChipType.eNone)
+            {
+                moveY++;
+            }
             this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[1];
         }
         else if (Input.GetButtonDown("Left Arrow"))
         {
             Debug.Log("左");
-            //this.gameObject.transform.position += new Vector3(-1, 0, 0);
+            if (field.GetNextChipType(moveX - 1, moveY) == EChipType.eNone)
+            {
+                moveX--;
+            }
             this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[2];
         }
         else if (Input.GetButtonDown("Right Arrow"))
         {
             Debug.Log("右");
-            //this.gameObject.transform.position += new Vector3(1, 0, 0);
+            if (field.GetNextChipType(moveX + 1, moveY) == EChipType.eNone)
+            {
+                moveX++;
+            }
             this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[3];
         }
 
-        //OutPutSpriteType();
+        // プレイヤーの位置を更新
+        this.gameObject.transform.position = new Vector3(offsetX + moveX, offsetY + moveY, 0);
     }
-
-    //private void OutPutSpriteType()
-    //{
-    //    // 使われているSpriteをリストアップ
-    //    var bound = tilemap.cellBounds;
-    //    var spriteList = new List<Sprite>();
-    //    for (int y = bound.max.y - 1; y >= bound.min.y; --y)
-    //    {
-    //        for (int x = bound.min.x; x < bound.max.x; ++x)
-    //        {
-    //            var tile = tilemap.GetTile<Tile>(new Vector3Int(x, y, 0));
-    //            if (tile != null && !spriteList.Contains(tile.sprite))
-    //            {
-    //                spriteList.Add(tile.sprite);
-    //            }
-    //        }
-    //    }
-    //    // どの場所でそのSpriteが使われているかを出力
-    //    var builder = new StringBuilder();
-    //    for (int y = bound.max.y - 1; y >= bound.min.y; --y)
-    //    {
-    //        for (int x = bound.min.x; x < bound.max.x; ++x)
-    //        {
-    //            var tile = tilemap.GetTile<Tile>(new Vector3Int(x, y, 0));
-    //            if (tile == null)
-    //            {
-    //                builder.Append("_,");
-    //            }
-    //            else
-    //            {
-    //                //var index = spriteList.IndexOf(tile.sprite);
-    //                string str = Regex.Replace(tile.sprite.name, @"[^0-9]", "");
-    //                int index = int.Parse(str);
-    //                builder.Append(index + ",");
-    //            }
-    //        }
-    //        builder.Append("\n");
-    //    }
-    //    Debug.Log(builder.ToString());
-    //}
 }
