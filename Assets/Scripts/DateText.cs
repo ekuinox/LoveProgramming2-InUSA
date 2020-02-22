5328fa990ee74c2b7c3718f828915eac752b94d1
takuteku <taku.0927.topublish@gmail.com>
2020-02-22 14:34:50 +0900

    カレンダーの日数を変更するスクリプトの作成

---
+++ Assets/Scripts/DateText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DateText : MonoBehaviour
+{
+    private Text date;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        date = this.gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
