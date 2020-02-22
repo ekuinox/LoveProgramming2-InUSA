11a204b6215bafdec8ad96fef6c34fcb07004deb
ekuinox <depkey@me.com>
2020-02-22 10:44:55 +0900

    ゲームクリアとオーバのイベント追加

---
+++ Assets/Scripts/GameClearEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GameClearEvent : LoveEvent
+{
+    public void init(Dictionary<string, string> args)
+    {
+
+    }
+
+    public void run(MessageText messageText)
+    {
+
+    }
+}
