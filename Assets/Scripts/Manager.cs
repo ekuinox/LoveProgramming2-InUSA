cdbdc0ad94ecdf8e681e89b5de87f57ca7ef90ee
ekuinox <depkey@me.com>
2020-02-22 10:15:51 +0900

    マネージャクラス

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public static class Manager
+{
+    private static bool isLoaded = false;
+
+    private static Message[] messages;
+
+    static int unityLovePoint = 0;
+    
+    static int ue4LovePoint = 0;
+
+    public static void Load(bool isForce = false)
+    {
+        if (!isLoaded || isForce)
+        {
+            messages = MessageLoader.GetFromFile($"{Application.dataPath}/messages.json").ToArray();
+            EventLoader.GetFromFile($"{Application.dataPath}/events.json");
+        }
+    }
+   
+}
