cf58b22617014fa3db15c717e55b466ad805fa69
takuteku <taku.0927.topublish@gmail.com>
2020-02-22 13:06:53 +0900

    シーン遷移用のスクリプトを作成(Staticで作ったからSceneControllerから呼んで)
    読み込みはLoadScene、アタッチはAttachScene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum ESceneState
+{
+    eTitle,
+    eSanFrancisco,
+    eLosAngeles,
+    eGalGame
+}
+
+static public class SceneController
+{
+
+    /*------------------------------------------
+     * LoadScene  : シーンの読み込み
+     * state      : 読み込むシーンステート
+     -------------------------------------------*/
+    static public void LoadScene(ESceneState state)
+    {
+        int index = GetSceneIndex(state);
+        SceneManager.LoadScene(index, LoadSceneMode.Single);
+    }
+
+    /*------------------------------------------
+     * AttachScene  : シーンのアタッチ
+     * state        : アタッチするシーンステート
+     -------------------------------------------*/
+    static public void AttachScene(ESceneState state)
+    {
+        int index = GetSceneIndex(state);
+        SceneManager.LoadScene(index, LoadSceneMode.Additive);
+    }
+
+    /*------------------------------------------
+     * GetSceneIndex  : シーンの番号を取得
+     * state          : 取得したいシーンのステート
+     * 戻り値         : シーンの番号
+     -------------------------------------------*/
+    static private int GetSceneIndex(ESceneState state)
+    {
+        switch (state)
+        {
+            case ESceneState.eTitle:
+                return 0;
+
+            case ESceneState.eSanFrancisco:
+                return 1;
+
+            case ESceneState.eLosAngeles:
+                return 2;
+
+            case ESceneState.eGalGame:
+                return 3;
+
+            default:
+                return -1;
+        }
+    }
+}
